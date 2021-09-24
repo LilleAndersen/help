95f6e3d57a0f955afc100ea8513f266558e20625
Little <71332089+LilleAndersen@users.noreply.github.com>
2021-09-24 14:38:49 +0200

    6
    faq question click and loading animation
    
    Co-Authored-By: Soni <mail.soni801@gmail.com>
    Co-Authored-By: MRcat77 <71332009+MRcat77@users.noreply.github.com>

--- js/faq.js
+++ js/faq.js
@@ -1,15 +1,42 @@
 fetch("/assets/json/faq.json")
-    .then(response=>response.json())
-    .then(data=>
+    .then(response => response.json())
+    .then(data =>
+    {
+        for(const question of data)
         {
-            for(const question of data)
-            {
-                document.querySelector("#questions").innerHTML+=`
-                    <div>
-                        <h3>${question.question}</h3>
-                        <p>${question.answer}</p>
-                    </div>
-                `;
-            }
+            document.querySelector("#questions").innerHTML+=`
+                <div>
+                    <h3>${question.question}</h3>
+                    <p>${question.answer}</p>
+                </div>
+            `;
         }
-    )
+    }).then(() =>
+    {
+        document.querySelectorAll("#questions h3").forEach(function (e, index)
+        {
+            setTimeout(function ()
+            {
+                e.style.opacity = "1";
+
+                const next = e.nextElementSibling;
+                next.style.marginBottom = `-${next.offsetHeight}px`;
+
+                e.addEventListener("click", function ()
+                {
+                    if (next.style.transform === "none")
+                    {
+                        next.style.transform = "translateY(-100%)";
+                        next.style.marginBottom = `-${next.offsetHeight}px`;
+                        next.style.opacity = "0";
+                    }
+                    else
+                    {
+                        next.style.transform = "none";
+                        next.style.marginBottom = "0";
+                        next.style.opacity = "1";
+                    }
+                });
+            }, index * 30);
+        });
+    });
