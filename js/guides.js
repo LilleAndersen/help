5b87ec8327237d1a52ead96c572cfa882dfdd8f6
Henrik X <e-hennep@asvg.no>
2021-11-24 10:35:33 +0100

    did the guides fix. now it just needs link to guides

--- js/guides.js
+++ js/guides.js
@@ -1,12 +1,42 @@
 // TODO: sort the guides based on category
+const guidenr = new URLSearchParams(window.location.search)
+
 fetch("/assets/json/guides.json")
     .then(response=>response.json())
     .then(data=>
         {
+            //Sidebar load from json
+            // Categories: Utilities, hey henrik i used arch btw
+            for(const guide of data) {
+                document.querySelector("#side-bar").innerHTML += `
+                        <a href="?g=${guide.id}">${guide.title}</a>
+                    `;
+            }
+
+            //URL parameter
+            const songinfo =  guidenr.get("g")
+
+            //Guide loading
             for(const guide of data)
             {
-                document.querySelector("#side-bar").innerHTML+=`
-                    <a href="${guide.url}">${guide.title}</a>
-                `;
+                if(guide.id.toString()==="home") {
+                    document.querySelector("#guide-content").innerHTML = `
+                        <h1>Velkommen til våre guidesguides</h1>
+                        <p>
+                            På denne siden skal vi lære deg lette og avanserte konsepter for at du skal kunne programmere. 
+                            Du kan også finne guides for spesifikk programmvare. Om du har ett lite spørsmål, så kan du sjekke ut 
+                            <a href="/faq/">FAQ</a>, fordi denne siden kommer til å være mer i dybden. Til å begynne med, så kan du 
+                            velge en guide fra menyen til venstre.
+                        </p>`
+                }
+                else if(guide.id.toString()===songinfo) {
+
+
+                    document.querySelector("#guide-content").innerHTML = `                
+                        <h1>Guide for ${guide.title}</h1>
+                        <iframe id="iframe" src="${guide.url}"></iframe>
+                    `
+                }
             }
-        });
+
+        })
--- temp/bodge.js
+++ temp/bodge.js
@@ -7,26 +7,3 @@
 // mrcat, more like will ignore you
 // henri moosic man
 // you're all mega intelligay
-
-
-//TODO: this stuff is gonna be responsible for the individual guide html stuff. Good luck soldier
-const guide = new URLSearchParams(window.location.search)
-
-
-
-fetch("/assets/json/guides.json").then(response=>response.json()).then(data=>{
-
-    const guideinfo =  guide.get("guide-id")
-
-    for(const guide of data) {
-        if(guide.id.toString()===guideinfo) {
-            document.querySelector("#more-guideinfo").innerHTML=`
-
-
-            `
-        }
-
-    }
-
-
-})
